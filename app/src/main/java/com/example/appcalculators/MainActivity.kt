35a652ac7014de9866338f52134f1280d9b4abd6
hiwansyah <iwansyy@gmail.com>
2020-11-11 09:58:08 +0700

    add padding

--- app/src/main/java/com/example/appcalculators/MainActivity.kt
+++ app/src/main/java/com/example/appcalculators/MainActivity.kt
@@ -12,53 +12,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_zero.setOnClickListener { appendVal("0", false) }
-        button_1.setOnClickListener { appendVal("1", false) }
-        button_2.setOnClickListener { appendVal("2", false) }
-        button_3.setOnClickListener { appendVal("3", false) }
-        button_4.setOnClickListener { appendVal("4", false) }
-        button_5.setOnClickListener { appendVal("5", false) }
-        button_6.setOnClickListener { appendVal("6", false) }
-        button_7.setOnClickListener { appendVal("7", false) }
-        button_8.setOnClickListener { appendVal("8", false) }
-        button_9.setOnClickListener { appendVal("9", false) }
-        button_zero_zero.setOnClickListener { appendVal("00", false) }
+        btnZero.setOnClickListener { appendVal("0", false) }
+        btnOne.setOnClickListener { appendVal("1", false) }
+        btnTwo.setOnClickListener { appendVal("2", false) }
+        btnThree.setOnClickListener { appendVal("3", false) }
+        btnFour.setOnClickListener { appendVal("4", false) }
+        btnFive.setOnClickListener { appendVal("5", false) }
+        btnSix.setOnClickListener { appendVal("6", false) }
+        btnSeven.setOnClickListener { appendVal("7", false) }
+        btnEight.setOnClickListener { appendVal("8", false) }
+        btnNine.setOnClickListener { appendVal("9", false) }
+        btnZerozero.setOnClickListener { appendVal("00", false) }
 
         // Operators
-        button_plus.setOnClickListener { appendVal("+", false) }
-        button_minus.setOnClickListener { appendVal("-", false) }
-        button_multiply.setOnClickListener { appendVal("*", false) }
-        button_divide.setOnClickListener { appendVal("/", false) }
-        button_zero_zero.setOnClickListener { appendVal("00", false) }
-        button_mod.setOnClickListener { appendVal("%", false) }
-        button_dot.setOnClickListener { appendVal(".", false) }
+        btnPlus.setOnClickListener { appendVal("+", false) }
+        btnMinus.setOnClickListener { appendVal("-", false) }
+        btnMultiply.setOnClickListener { appendVal("*", false) }
+        btnDivide.setOnClickListener { appendVal("/", false) }
+        btnZerozero.setOnClickListener { appendVal("00", false) }
+        btnMod.setOnClickListener { appendVal("%", false) }
+        btnDot.setOnClickListener { appendVal(".", false) }
 
-        button_delete.setOnClickListener {
-            val expression = cal_expression.text.toString()
+        btnDelete.setOnClickListener {
+            val expression = callExpression.text.toString()
             if (expression.isNotEmpty()) {
-                cal_expression.text = expression.substring(0, expression.length - 1)
+                callExpression.text = expression.substring(0, expression.length - 1)
             }
         }
 
-        button_C.setOnClickListener {
-            cal_expression.text = ""
-            cal_result.text = ""
+        btnC.setOnClickListener {
+            callExpression.text = ""
+            callResult.text = ""
         }
 
-        button_equal.setOnClickListener {
+        btnEqual.setOnClickListener {
             try {
-                val expression = ExpressionBuilder(cal_expression.text.toString()).build()
+                val expression = ExpressionBuilder(callExpression.text.toString()).build()
                 val result = expression.evaluate()
                 val longResult = result.toLong()
                 if (result == longResult.toDouble()) {
 
-                    cal_result.text = longResult.toString()
+                    callResult.text = longResult.toString()
                 } else
-                    cal_result.text = result.toString()
+                    callResult.text = result.toString()
 
             } catch (e: Exception) {
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-
                 Log.d("EXCEPTION", "Message: ${e.message}")
             }
         }
@@ -66,10 +65,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun appendVal(string: String, isClear: Boolean) {
         if (isClear) {
-            cal_expression.text = ""
-            cal_expression.append(string)
+            callExpression.text = ""
+            callExpression.append(string)
         } else {
-            cal_expression.append(string)
+            callExpression.append(string)
         }
     }
 }
